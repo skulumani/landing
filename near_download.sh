93886acd96394c72aa1e2ee6a4d97cfaeb01f56a
Shankar Kulumani <shanks.k@gmail.com>
2017-05-26 14:17:53 -0400

    Trying to get wget to work properly

--- near_download.sh
+++ near_download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+NEAR_URL="http://naif.jpl.nasa.gov/pub/naif/pds/data/near-a-spice-6-v1.0"
 # download the NEAR SPICE data to the current directory
-wget -m -nH --cut-dirs=5 -nv http://naif.jpl.nasa.gov/pub/naif/pds/data/near-a-spice-6-v1.0/nearsp_1000
+wget --mirror --no-host-directories --no-verbose --show-progress --wait=10 --random-wait $NEAR_URL
