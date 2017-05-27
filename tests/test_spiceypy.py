4fc2abf0155c9d42405d71dea0c3dd91ad9b910c
Shankar Kulumani <shanks.k@gmail.com>
2017-05-27 14:25:43 -0400

    Remove debugger in test function

--- tests/test_spiceypy.py
+++ tests/test_spiceypy.py
@@ -16,7 +16,6 @@ def test_spiceypy_installation_correct():
     np.testing.assert_equal(spice.tkvrsn('TOOLKIT'),spice_version)
 
 def test_spiceypy_cassini(tmpdir):
-    pdb.set_trace()
     download_cassini_spice()
     step = 4000
     utc = ['Jun 20, 2004', 'Dec 1, 2005']
@@ -28,5 +27,4 @@ def test_spiceypy_cassini(tmpdir):
     times = [x*(etTwo-etOne)/step + etOne for x in range(step)]
 
     positions, lightTimes = spice.spkpos('Cassini', times, 'J2000', 'None', 'SATURN BARYCENTER')
-    pdb.set_trace()
     np.testing.assert_array_almost_equal(positions[0], true_initial_pos)
