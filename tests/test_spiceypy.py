cc1091f428c205fcec1c9cba63718bc14235dee0
Shankar Kulumani <shanks.k@gmail.com>
2017-05-30 17:46:16 -0400

    Add some testing which uses NEAR kernels

--- tests/test_spiceypy.py
+++ tests/test_spiceypy.py
@@ -9,22 +9,52 @@ import pdb
 
 from spice_test import download_cassini_spice
 
-true_initial_pos = [-5461446.61080924 ,-4434793.40785864 ,-1200385.93315424]
-
 def test_spiceypy_installation_correct():
     spice_version = 'CSPICE_N0065'
     np.testing.assert_equal(spice.tkvrsn('TOOLKIT'),spice_version)
 
-def test_spiceypy_cassini(tmpdir):
+class TestSpiceyPyFunctions():
     download_cassini_spice()
-    step = 4000
     utc = ['Jun 20, 2004', 'Dec 1, 2005']
 
     etOne = spice.str2et(utc[0])
     etTwo = spice.str2et(utc[1])
-    print("ET One: {}, ET Two: {}".format(etOne, etTwo))
+    step = 4000
 
     times = [x*(etTwo-etOne)/step + etOne for x in range(step)]
 
-    positions, lightTimes = spice.spkpos('Cassini', times, 'J2000', 'None', 'SATURN BARYCENTER')
-    np.testing.assert_array_almost_equal(positions[0], true_initial_pos)
+    def test_spiceypy_cassini(self):
+        true_initial_pos = [-5461446.61080924 ,-4434793.40785864 ,-1200385.93315424]
+        positions, lightTimes = spice.spkpos('Cassini', self.times, 'J2000', 'None', 'SATURN BARYCENTER')
+        np.testing.assert_array_almost_equal(positions[0], true_initial_pos)
+
+    def test_spicepy_rotation_matrix_identity(self):
+        R_E2E = spice.pxform('IAU_EARTH', 'IAU_EARTH', self.etOne)
+        np.testing.assert_array_almost_equal(R_E2E, np.eye(3))
+
+    def test_spicepy_state_transformation(self):
+        T = spice.sxform('IAU_EARTH', 'IAU_SATURN', self.etOne)
+        R = spice.pxform('IAU_EARTH', 'IAU_SATURN', self.etOne)
+        (Rout, wout) = spice.xf2rav(T)
+        np.testing.assert_array_almost_equal(Rout, R)
+
+class TestNEARKernels():
+    spice.furnsh('./near_2001.tm')
+    ckid = spice.ckobj('./nearsp_1000/data/ck/near_20010101_20010228_v01.bc')[0]
+    cover = spice.ckcov('./nearsp_1000/data/ck/near_20010101_20010228_v01.bc', 
+            ckid, False, 'INTERVAL', 0.0, 'SCLK')
+    
+    def test_near_body_frames(self):
+        """Transformation from Body fixed frame to prime frame
+
+        There is a constant rotation of 135 deg about the Z/Third axis
+        """
+        R, av, clkout = spice.ckgpav(self.ckid, self.cover[0], 0, 'NEAR_SC_BUS')
+        ang = 135*np.pi/180
+        R_act = np.array([[np.cos(ang), -np.sin(ang), 0], 
+                          [np.sin(ang), np.cos(ang), 0],
+                          [0, 0, 1]])
+
+        np.testing.assert_array_almost_equal(R, R_act)
+        np.testing.assert_array_almost_equal(av, np.zeros(3))
+        np.testing.assert_almost_equal(clkout, self.cover[0])
