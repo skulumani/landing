04e6c7797f7245d2cc6b820e991774cae9bf51cb
Shankar Kulumani <shanks.k@gmail.com>
2017-06-05 23:18:26 -0400

    New image class for near imagery

--- images.py
+++ images.py
@@ -1,5 +1,10 @@
 """This module will download imagery for Near
 """
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from astropy.io import fits
+import spiceypy as spice
+
 import sys
 import os
 import urlparse
@@ -19,50 +24,112 @@ directory = 'images'
 if not os.path.isdir(os.path.join(cwd,directory)):
     os.mkdir(os.path.join(cwd,directory))
 
-def getNearMSI(year='2001', day='043'):
-    """Download all the MSI imagery from a specific day of flight
-
-    This function will download all the MSI imagery from 
-    the chosen day. If the day is not valid the response will fail.
-
-    Only downloads the IOFDBL images (Deblurred I/F Images)
-
-    https://sbn.psi.edu/archive/near/NEAR_A_MSI_3_EDR_EROS_ORBIT_V1_0/catalog/msi2erosds.cat
-
-    All the MSI data is available here:
-
-    https://sbn.psi.edu/archive/near/NEAR_A_MSI_3_EDR_EROS_ORBIT_V1_0/
+class NearImages(object):
+    """Class that holds all the image data for Near landing
     """
-    url = 'https://sbn.psi.edu/archive/near/NEAR_A_MSI_3_EDR_EROS_ORBIT_V1_0/data/' + year + '/' + day + '/' + 'iofdbl/'
 
-    path = os.path.join(cwd, directory)
-    try:
-        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
-        ii = 0
-        request = urllib2.Request(url, None, headers)
-        html = urllib2.urlopen(request)
-        soup = BeautifulSoup(html.read(), 'lxml')
+    def __init__(self, year='2001', day='043', downloaded=False):
+        """Download all the MSI imagery from a specific day of flight
+
+        This function will download all the MSI imagery from 
+        the chosen day. If the day is not valid the response will fail.
+
+        Only downloads the IOFDBL images (Deblurred I/F Images)
+
+        https://sbn.psi.edu/archive/near/NEAR_A_MSI_3_EDR_EROS_ORBIT_V1_0/catalog/msi2erosds.cat
+
+        All the MSI data is available here:
+
+        https://sbn.psi.edu/archive/near/NEAR_A_MSI_3_EDR_EROS_ORBIT_V1_0/
+        """
+        url = 'https://sbn.psi.edu/archive/near/NEAR_A_MSI_3_EDR_EROS_ORBIT_V1_0/data/' + year + '/' + day + '/' + 'iofdbl/'
+
+        path = os.path.join(cwd, directory)
+        self.path = path
+        if not downloaded:
+            try:
+                headers = {"User-Agent": ("Mozilla/5.0 (X11; Linux x86_64)"
+                        " AppleWebKit/537.36 (KHTML, like Gecko)"
+                        " Chrome/58.0.3029.110 Safari/537.36")}
+                ii = 0
+                request = urllib2.Request(url, None, headers)
+                html = urllib2.urlopen(request)
+                soup = BeautifulSoup(html.read(), 'lxml')
+                
+                for tag in soup.findAll('a', href=True):
+                    href_url = urlparse.urljoin(url, tag['href'])
+                    ext = os.path.splitext(os.path.basename(href_url))[1]
+                    if ext == '.fit' or ext == '.lbl':
+                        current = urllib2.urlopen(href_url)
+                        local_filename = os.path.basename(href_url)
+                        local_filepath = os.path.join(path, local_filename)
+
+                        if not os.path.isfile(local_filepath):                
+                            print("Downloading: {}".format(local_filename))
+                            f = open(local_filepath, 'wb')
+                            f.write(current.read())
+                            f.close()
+                            ii = ii + 1
+                        else:
+                            print("Skipping: {}".format(local_filename))
+                    
+                print("Download {} files".format(ii))
+                print("All Images from day {0} of {1} should be present".format(
+                    day, year))
+            except KeyboardInterrupt:
+                print("Stopping the download process")
         
-        for tag in soup.findAll('a', href=True):
-            href_url = urlparse.urljoin(url, tag['href'])
-            ext = os.path.splitext(os.path.basename(href_url))[1]
-            if ext == '.fit' or ext == '.lbl':
-                current = urllib2.urlopen(href_url)
-                local_filename = os.path.basename(href_url)
-                local_filepath = os.path.join(path, local_filename)
+        # now store all the filenames into an array
+        self.fits = sorted([f for f in os.listdir(path) if f[-3:] == 'fit'])
+        self.lbls = sorted([f for f in os.listdir(path) if f[-3:] == 'lbl'])
+    
+        # useful keys for header
+        self.xaxis_key = 'NAXIS1'
+        self.yaxis_key = 'NAXIS2'
+        self.expms_key = 'NEAR-010'
+        self.teltemp_key = 'NEAR-015'
+        self.ccdtemp_key = 'NEAR-016'
+        self.sclkstart_key = 'NEAR-017'
+        self.sclkmid_key = 'NEAR-018'
+        # first element is the scalar. quat_key
+        self.quat_key = ['NEAR-019', 'NEAR-020', 'NEAR-021', 'NEAR-022']
 
-                if not os.path.isfile(local_filepath):                
-                    print("Downloading: {}".format(local_filename))
-                    f = open(local_filepath, 'wb')
-                    f.write(current.read())
-                    f.close()
-                    ii = ii + 1
-                else:
-                    print("Skipping: {}".format(local_filename))
+    def extract_image_data(self, near, image=''):
+        """Extract image data from the Fits
+        """
+        # preallocate a dictionary/array to store all the information from each
+        # image
+        spice.furnsh(near.metakernel)
+        self.images = []
+        # loop over all the images
+        for ii, f in enumerate(self.fits): 
+            data = fits.open(os.path.join(self.path, f), memmap=True)
+            image_data = data[0].data
+            header = data[0].header
+            data.close()
 
-        print("Download {} files".format(ii))
-        return 0
-    except KeyboardInterrupt:
-        print "Exiting"
-        return 1
+            # extract the useful information for each image
+            image_data = {'image': image_data,
+                    'header': header,
+                    'path': os.path.join(self.path, f),
+                    'order': ii,
+                    'quat': [header[x] for x in self.quat_key],
+                    'met_sec': header[self.sclkmid_key],
+                    'et': spice.sct2e(int(near.near_id), 
+                        header[self.sclkmid_key]),
+                    'utc': spice.timout(spice.sct2e(int(near.near_id), 
+                        header[self.sclkmid_key]),
+                        'YYYY MON DD HR:MN:SC.#### (TDB) ::TDB'),
+                    'exposure': header[self.expms_key],
+                    'telescope_temp': header[self.teltemp_key],
+                    'ccd_temp': header[self.ccdtemp_key]
+                    }
+            self.images.append(image_data)
+        
+        spice.kclear()
+#             Ri2b_fits = spice.q2m(quat_fits)
+#             # get teh quaternion from teh kernels
+#             Ri2b_spice = spice.pxform(self.near.inertial_frame, 
+#                     self.near.near_body_frame, et)
+
 
